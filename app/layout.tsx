43cfd6dd5e8304778f932407a9c178e6a000685f
sarcasticcrab <64483359+sarcasticcrab@users.noreply.github.com>
2026-04-09 13:39:13 +0100

    Formatting for mobile view

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=1200" />
         <link rel="preconnect" href="https://use.typekit.net" crossOrigin="anonymous" />
         <link rel="stylesheet" href="https://use.typekit.net/wdn6pox.css" />
       </head>
